ea03594b0e15b638595f702ea829fd17dd0e3abf
minzdeflaz <bv88@drexel.edu>
2021-06-07 15:49:16 +0700

    Use freemysqlhosting setup

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 #MySQL setup:
-username = ''
-password = ''
-server = ''
-database = ''
+username = 'sql6417619'
+password = 'UuUG3TsNZv'
+server = 'sql6.freemysqlhosting.net'
+database = 'sql6417619'
 URI = 'mysql+pymysql://' + username + ':' + password + '@' + server + '/' + database
 
 #Secret Key
-Secret = ''
+Secret = 'dontstealthis'
